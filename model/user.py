63d8027d977704ca8dc81a4473e8dd154dbe3e17
Sergio Majé <smajefranco@gmail.com>
2021-12-01 16:27:29 -0500

    :art: User model
    SQLAlchemy structure

--- model/user.py
+++ model/user.py
@@ -1,17 +1,30 @@
-from datetime import datetime
-from typing import Optional
 from pydantic import BaseModel
+from sqlalchemy import (
+    Column,
+    Integer,
+    Boolean,
+    String,
+    Date
+)
+
+from base import Base
 
 
-class User(BaseModel):
-    id: Optional[int] = None
-    name: str
-    alias: str
-    password: str
-    date_birth: datetime
-    email: str
-    phone: str
-    potential: int
-    sex: bool
-    foreground: str
-    background: str
+class User(Base, BaseModel):
+    __tablename__ = 'user'
+
+    id = Column(Integer,
+                primary_key=True,
+                unique=True,
+                nullable=False,
+                autoincrement=True)
+    name = Column(String, nullable=False)
+    alias = Column(String, unique=True, nullable=False)
+    password = Column(String, unique=True, nullable=False)
+    date_birth = Column(Date, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    phone = Column(String, unique=True, nullable=False)
+    potential = Column(Integer, nullable=False)
+    sex = Column(Boolean, nullable=False)
+    foreground = Column(String, nullable=False)
+    background = Column(String, nullable=False)
