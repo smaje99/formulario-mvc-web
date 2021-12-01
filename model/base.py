344bfc60cc33851c05d5bccce07054b98b6cfbe9
Sergio Majé <smajefranco@gmail.com>
2021-12-01 16:12:48 -0500

    :card_file_box: Database config

---
+++ model/base.py
@@ -0,0 +1,36 @@
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('sqlite:///users.db')
+
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+@contextmanager
+def session_readings():
+    session = Session()
+    try:
+        yield session
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+@contextmanager
+def session_writing():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
