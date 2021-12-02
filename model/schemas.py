d95ed5719aea8fcf57ebd3df0650384fd7c81ddf
Sergio Majé <smajefranco@gmail.com>
2021-12-01 19:41:12 -0500

    :sparkles: User schema
    Pydantic schema

---
+++ model/schemas.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: Optional[int]
+    name: str
+    alias: str
+    password: str
+    date_birth: datetime
+    email: str
+    phone: str
+    potential: int
+    sex: bool
+    foreground: str
+    background: str
